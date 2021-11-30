0e6881398d3f953a8971a968fc40882c987e14d8
uhrga <uhrga@SCC-POOL-F-14>
2021-11-30 09:32:33 +0100

    changes

--- RP/W5/W5A2.java
+++ RP/W5/W5A2.java
@@ -1,25 +1,34 @@
 package RP.W5;
 
 public class W5A2 {
-    static int[][][] feld = new int[][][]{{{1,2},{9,8,7,6},{1,1,1}},{{1,2,5},{1}}};
+    static int[][][] feld = new int[][][] { { { 1, 2 }, { 9, 8, 7, 6 }, { 1, 1, 1 } }, { { 1, 2, 5 }, { 1 } } };
+
     public static void main(String[] args) {
-        
+        int[][][] feldII = deepcopyII(feld);
+        for (int i = 0; i < feldII.length; i++) {
+            for (int j = 0; j < feldII[i].length; j++) {
+                for (int j2 = 0; j2 < feldII[i][j].length; j2++) {
+                    System.out.println(feldII[i][j][j2]);
+                }
+            }
+        }
     }
-    public static int[][][] feldkopierenfalch(int[][][] array){
+
+    public static int[][][] feldkopierenfalch(int[][][] array) {
         return array;
     }
 
-    public static int[][][] deepcopyI(int[][][] array){
+    public static int[][][] deepcopyI(int[][][] array) {
         return array.clone();
     }
-    
+
     public static int[][][] deepcopyII(int[][][] array) {
         int[][][] feld = new int[array.length][][];
         for (int i = 0; i < array.length; i++) {
             feld[i] = new int[array[i].length][];
-            for (int j = 0; j < array.length; j++) {
-                feld[i][j] = new int[array[j].length];
-                for (int j2 = 0; j2 < array.length; j2++) {
+            for (int j = 0; j < array[i].length; j++) {
+                feld[i][j] = new int[array[i][j].length];
+                for (int j2 = 0; j2 < array[i][j].length; j2++) {
                     feld[i][j][j2] = array[i][j][j2];
                 }
             }
--- RP/W5/W5A4/Telefonbuch.java
+++ RP/W5/W5A4/Telefonbuch.java
@@ -8,68 +8,90 @@ public class Telefonbuch {
     static HashMap<Integer, Entry> entriesID = new HashMap<Integer, Entry>();
     static boolean run = true;
     static Scanner scanner;
-    
+
     public static void main(String[] args) {
         init();
-        while(run)runtime();
+        while (run)
+            runtime();
     }
-    public static void init(){
+
+    public static void init() {
         scanner = new Scanner(System.in);
     }
 
-    public static void runtime(){
+    public static void runtime() {
         System.out.print("Telefonbuch comand >..");
         String[] input = scanner.nextLine().split(" ");
-        
-        switch (input[0]){
-            case "shutdown" -> shutdown();
-            case "add" -> add(input);
-            case "read" -> read(input);
-            case "printbook" -> printTelephonebook();
-            default -> System.out.println("This comand doesnt make any sense dude. Please repeat! \n");
+
+        switch (input[0]) {
+            case "shutdown":
+                shutdown();
+                break;
+            case "add":
+                add(input);
+                break;
+            case "read":
+                read(input);
+                break;
+            case "printbook":
+                printTelephonebook();
+                break;
+            case "help":
+                help();
+                break;
+            default:
+                System.out.println("This comand doesnt make any sense dude. Please repeat! \n");
         }
 
     }
 
-    public static void shutdown(){
+    public static void shutdown() {
         run = false;
         scanner.close();
         System.out.println("Shutdown programm");
     }
-    
-    
-    public static void add(String[] input){
-        if (input.length > 3){
+
+    public static void add(String[] input) {
+        if (input.length > 3) {
             try {
                 new Entry(input[1], input[2], Integer.parseInt(input[3]));
             } catch (NumberFormatException e) {
                 System.out.println("This telephonenumber format is invalid!");
             }
-            
+
         }
-        
+
     }
 
     public static void read(String[] input) {
-        
+
         if (input.length > 2) {
-            if (entriesName.containsKey((input[1] + " "  + input[2]))){
+            if (entriesName.containsKey((input[1] + " " + input[2]))) {
                 System.out.println(entriesName.get(input[1] + " " + input[2]));
-            }
-            else{
+            } else {
                 System.out.println("This person doesnt exist.");
             }
-            
-        }else{
+
+        } else {
             System.out.println("This comand does not have the right parameters");
         }
 
     }
 
-    public static void printTelephonebook(){
+    public static void printTelephonebook() {
         System.out.println("Whole telephonebook: ");
         for (String key : entriesName.keySet()) {
             System.out.println(entriesName.get(key));
         }
     }
+
+    public static void help() {
+        String[][] help = new String[][] { { "help", "", "prints all comands and parameters" },
+                { "add", "Vorname", "Nachname", "Telfonnummer" } };
+        String format = "%-" + 48 + "s%-" + 48 + "s%-" + 48 + "s";
+        for (String[] strings : help) {
+            System.out.println(String.format(format, strings[0], strings[1], strings[2]));
+        }
+    }
+
 }
