56575d16d081cdca740bf722ee747e550d219b74
Thore Haupt <thore.haupt@t-online.de>
2022-01-17 19:35:08 +0100

    A3d

---
+++ RP/W7/A3/AbstammungDemo.java
@@ -0,0 +1,9 @@
+package RP.W7.A3;
+
+public class AbstammungDemo {
+    public static void main(String[] args) {
+        Luchs l = new Luchs();
+        System.out.println(l.getAbstammung());
+        
+    }
+}
