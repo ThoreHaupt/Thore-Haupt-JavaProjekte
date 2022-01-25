0b0633fa5af6434f3cea448d2520624b0f43f832
Thore Haupt <thore.haupt@t-online.de>
2022-01-25 08:01:20 +0100

    enums

---
+++ PracticeProjects/enumstuff.java
@@ -0,0 +1,37 @@
+package PracticeProjects;
+
+public class enumstuff {
+    enum Wochentage {
+        Montag(1, 2),
+        Diensatag(1, 2),
+        Mittwoch(1, 2),
+        Donnerstag(1, 2),
+        Freitag(1, 2),
+        Samstag(1, 2),
+        Sonntag(1, 2);
+
+        private double Stunden;
+        private double Sekunden;
+
+        Wochentage(double Stunden, double Sekunden) {
+            this.Stunden = Stunden;
+            this.Sekunden = Sekunden;
+        }
+
+        public double Stunden() {
+            return this.Stunden;
+        }
+
+    }
+
+    int[] liste = { 1, 2, 3, 4, 5 };
+
+    public static void main(String[] args) {
+        System.out.println(Wochentage.values());
+
+        for (Wochentage Tag : Wochentage.values()) {
+            System.out.println("" + Tag + ": " + Tag.Stunden());
+        }
+
+    }
+}
