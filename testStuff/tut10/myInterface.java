251bbc0633ef79216b5f8606d91e3f8cb5f0f7a7
Thore Haupt <thore.haupt@t-online.de>
2022-02-02 14:14:47 +0100

    shiesh

--- testStuff/tut10/myInterface.java
+++ testStuff/tut10/myInterface.java
@@ -3,6 +3,8 @@ package testStuff.tut10;
 public interface myInterface {
     public void myMethodA();
 
+    abstract void name();
+
     public static class test1 {
         public void myMethodC() {
             System.out.println("C");
@@ -23,6 +25,12 @@ public interface myInterface {
             obj.myMethodA();
             // obj.myMethodB();
             // obj.myMethodC();
+        }
+
+        @Override
+        public void name() {
+            // TODO Auto-generated method stub
+
         }
     }
 }
