bac163315e0f2968ff63a8bdfc2d2c1341f0b28b
Thore Haupt <thore.haupt@t-online.de>
2022-02-03 23:32:14 +0100

    ArrayList implementation brrr

--- PracticeProjects/ArrayListStuff/TArrayList.java
+++ PracticeProjects/ArrayListStuff/TArrayList.java
@@ -1,18 +1,22 @@
 package PracticeProjects.ArrayListStuff;
 
 public class TArrayList<T> {
-    Object[] array;
-    int arrSize = 0;
-    int lastIndex = 0;
-    Class<T> type;
+    private Object[] array;
+    private int arrSize = 0;
+    private int lastIndex = 0;
+    // final Class<T> type;
 
     public static void main(String[] args) {
         TArrayList<String> list = new TArrayList<String>();
         list.add("1");
         list.add("2");
         list.add("3");
-        list.insert("element", 2);
-        list.print();
+        list.insert("element", 1);
+        System.out.println(list.print());
+        for (int i = 0; i < 20; i++) {
+            list.add("" + i);
+        }
+        System.out.println(list.print());
 
     }
 
@@ -23,35 +27,69 @@ public class TArrayList<T> {
     }
 
     public TArrayList() {
-        this.arrSize = 10;
-        this.array = new Object[10];
+        this(10);
     }
 
     public TArrayList(int startSize) {
+        this.arrSize = startSize;
         this.array = new Object[startSize];
+        // type = (Class<T>) ((ParameterizedType)
+        // getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 
     public TArrayList(Object[] arr) throws wrongTypeException {
-        for (int i = 0; i < arr.length; i++) {
-            if (type.isInstance(arr[i]))
-                throw new wrongTypeException("Type is" + type.toString());
-        }
+        /*
+         * for (int i = 0; i < arr.length; i++) {
+         * 
+         * if (type.isInstance(arr[i]))
+         * throw new wrongTypeException("Type is" + type.toString());
+         * 
+         * }
+         */
         this.array = arr;
     }
 
     public void add(T element) {
-        sizeManager(1);
-        array[lastIndex] = element;
+        sizeCheck(1);
+        array[lastIndex++] = element;
+    }
+
+    public void add(T[] arr) {
+        sizeCheck(arr.length);
+        for (T t : arr) {
+            add(t);
+        }
+    }
+
+    public void add(TArrayList<T> list) {
+        sizeCheck(list.size());
+        for (T object : list.asArray()) {
+            this.add(object);
+        }
     }
 
     public void insert(T element, int index) throws wrongTypeException {
         shiftArray(index, 1);
-        if (type.isInstance(element))
-            throw new wrongTypeException("Type is" + type.toString());
+        /*
+         * if (type.isInstance(element))
+         * throw new wrongTypeException("Type is" + type.toString());
+         */
         array[index] = element;
     }
 
-    private void sizeManager(int amount) {
+    public void insert(T[] array, int firstIndex) {
+        shiftArray(firstIndex, array.length);
+        for (int i = 0; i < array.length; i++) {
+            this.array[firstIndex + i] = array[i];
+        }
+    }
+
+    public T get(int index) throws IndexOutOfBoundsException {
+
+        return (T) array[index];
+    }
+
+    private void sizeCheck(int amount) {
         if (arrSize + amount < lastIndex - 1) {
             return;
         } else {
@@ -60,31 +98,49 @@ public class TArrayList<T> {
                 newArray[i] = array[i];
             }
             array = newArray;
+            arrSize *= 2;
             return;
         }
     }
 
     private TArrayList<T> shiftArray(int index, int amount) {
-        sizeManager(amount);
+        sizeCheck(amount);
         Object[] tempArray = new Object[amount];
         int tempLoc = 0;
         Object help;
+        lastIndex += amount;
         for (int i = index; i < lastIndex; i++) {
             tempLoc = (i - index) % amount;
             help = tempArray[tempLoc];
-            tempArray[tempLoc] = array[i];
+            tempArray[tempLoc] = (i >= lastIndex) ? null : array[i];
             array[i] = help;
         }
         return this;
     }
 
     public String print() {
-        String out = "";
-        for (int index = 0; index < array.length; index++) {
+        String out = "(";
+        for (int index = 0; index < lastIndex - 1; index++) {
             if (array[index] != null) {
-                out += array[index];
+                out += array[index] + ", ";
             }
         }
+        if (array[lastIndex - 1] != null) {
+            out += array[lastIndex - 1];
+        }
+        out += ")";
         return out;
+    }
+
+    public int size() {
+        return lastIndex;
+    }
+
+    public T[] asArray() {
+        Object[] returnarray = new Object[lastIndex];
+        for (int i = 0; i < returnarray.length; i++) {
+            returnarray[i] = array[i];
+        }
+        return (T[]) returnarray;
     }
 }
