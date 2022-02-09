a31507708e4a9b428a8486292e40396607dc0d34
Thore Haupt <thore.haupt@t-online.de>
2022-02-09 19:12:11 +0100

    baumaufgabe aus der Klausur

---
+++ PracticeProjects/Baum.java
@@ -0,0 +1,40 @@
+package PracticeProjects;
+
+public class Baum {
+    Baum links;
+    Baum rechts;
+    int wert;
+
+    public Baum(int wert) {
+        this.wert = wert;
+    }
+
+    public static boolean suche(Baum baum) {
+        System.out.println(baum.wert);
+        if (baum.wert == 0)
+            return true;
+        if (baum.links != null) {
+            /*
+             * if (suche(baum.links)) {
+             * return true;
+             * }
+             */
+            return suche(baum.links);
+        }
+        if (baum.rechts != null) {
+            if (suche(baum.rechts)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Baum b1 = new Baum(12);
+        b1.links = new Baum(123);
+        b1.rechts = new Baum(23);
+        b1.links.rechts = new Baum(0);
+        b1.links.rechts.links = new Baum(123);
+        System.out.println(suche(b1));
+    }
+}
