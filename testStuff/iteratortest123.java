bd6ed4286759f68687fc9871f839eee173a0b548
Thore Haupt <thore.haupt@t-online.de>
2022-05-03 11:09:44 +0200

    stoppwatch programm

--- testStuff/iteratortest123.java
+++ testStuff/iteratortest123.java
@@ -1,5 +1,6 @@
 package testStuff;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -7,6 +8,21 @@ import java.util.HashSet;
 public class iteratortest123 {
     public static void main(String[] args) {
         HashSet<Integer> h = new HashSet<Integer>();
+        ArrayList<Integer> m = new ArrayList<Integer>();
 
+        for (int i = 0; i < 100000; i++) {
+            h.add(Integer.valueOf(i));
+        }
+        for (int i = 0; i < 100000; i++) {
+            m.add(Integer.valueOf(i));
+        }
+        long current1 = System.nanoTime();
+        Object[] a = h.toArray();
+        double t1 = (System.nanoTime() - current1) * Math.pow(10, -6);
+        System.out.println(t1);
+        long current2 = System.nanoTime();
+        Object[] b = m.toArray();
+        double t2 = (System.nanoTime() - current2) * Math.pow(10, -6);
+        System.out.println(t2);
     }
 }
