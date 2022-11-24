0b0af5a8d2fb4bbd39301dc3bd1d80fa546523aa
Thore Haupt <thore.haupt@t-online.de>
2022-11-25 00:51:21 +0100

    changes to test and old RP

--- RP/W3/W3A1.java
+++ RP/W3/W3A1.java
@@ -1,22 +1,24 @@
 package RP.W3;
+
 import java.util.Scanner;
 
-public class W3A1{
+public class W3A1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         int input = scanner.nextInt();
         System.out.println("Alle Sekunden: " + input);
-        
-        int jahre = input/(24*3600*365);
-        int tage = input%(24*3600*365)/(3600*24);
-        int seks = input%(24*365*3600*jahre + (24 * 3600 * tage));
-        int stunden = seks/(3600);
-        int minuten = seks%3600/60;
-        int sekunden = seks%60;
 
+        int jahre = input / (24 * 3600 * 365);
+        int sekTagen = input % (24 * 3600 * 365);
+        int tage = sekTagen / (3600 * 24);
+        int seks = sekTagen % (3600 * 24);
+        int stunden = seks / (3600);
+        int secMinuten = seks % 3600;
+        int minuten = secMinuten / 60;
+        int sekunden = seks % 60;
 
         System.out.println("Jahre: " + jahre);
         System.out.println("Tage: " + tage);
