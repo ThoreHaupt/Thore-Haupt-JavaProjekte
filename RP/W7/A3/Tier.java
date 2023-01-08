6581361ab86c03dab27b73aedf6eab6eab7a5c40
Thore Haupt <thore.haupt@t-online.de>
2023-01-08 22:21:10 +0100

    fix für Object Hierachie druck

--- RP/W7/A3/Tier.java
+++ RP/W7/A3/Tier.java
@@ -3,34 +3,37 @@ package RP.W7.A3;
 public class Tier {
     public static void main(String[] args) {
         Tier t = new Tier();
-        t.getAbstammung();
-    }
-    
-    public String getAbstammung() {
-        String upper = "Object";
-        return this.getClass().getSimpleName() + " --> " + upper;
+
+        System.out.println(t.getAbstammung());
+        t.getAb();
     }
 
-    public void fressen(){
+    public String getAbstammung() {
+        String upper = "Object";
+        return "Tier" + " --> " + upper;
+    }
+
+    public void fressen() {
         System.out.println(this + ": Tier, fressen");
     }
 
-    public void gibLaut(){
+    public void gibLaut() {
         System.out.println(this + ": Tier, gibtLaut");
     }
 
     public void getAb() {
         //System.out.println(this.getClass().getSuperclass()
-                //.getSuperclass().getSuperclass().getSuperclass().getSuperclass().getSimpleName());
+        //.getSuperclass().getSuperclass().getSuperclass().getSuperclass().getSimpleName());
         //System.out.println(this.getClass().getName());
         Class<?> stufe = this.getClass();
         String output = "";
-        while(stufe.getSuperclass().getSuperclass() != null){
-            output += stufe.getSimpleName() + " --> ";
+        output += stufe.getSimpleName() + " --> ";
+        while (stufe.getSuperclass().getSuperclass() != null) {
             stufe = stufe.getSuperclass();
+            output += stufe.getSimpleName() + " --> ";
         }
         output += stufe.getSuperclass().getSimpleName();
         System.out.println(output);
     }
-    
+
 }
